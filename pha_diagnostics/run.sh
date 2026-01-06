d3f05c29a607cf60acb6273e7b83878e568122b3
psnDoc23 <dryerson@prophetsports.net>
2026-01-05 18:14:20 -0700

    Change server port from 8000 to 8099

--- pha_diagnostics/run.sh
+++ pha_diagnostics/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 echo "PHA Diagnostics add-on started"
 cd /usr/local/bin
-exec /venv/bin/python -m uvicorn server:app --host 0.0.0.0 --port 8000
+# exec /venv/bin/python -m uvicorn server:app --host 0.0.0.0 --port 8000
+exec python -m uvicorn server:app --host 0.0.0.0 --port 8099
