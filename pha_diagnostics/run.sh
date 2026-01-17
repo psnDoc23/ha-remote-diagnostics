09cd19415cf299c8d3f460f7dbd4ab6bea125c17
psnDoc23 <dryerson@prophetsports.net>
2026-01-17 14:02:21 -0700

    Update run.sh

--- pha_diagnostics/run.sh
+++ pha_diagnostics/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 echo "PHA Guardian Diagnostic add-on started"
 cd /usr/local/bin
-exec /venv/bin/python -m uvicorn server:app --host 0.0.0.0 --port 8099
+# per cop 20260117 changing back to 8000 because ingress is in place
+# exec /venv/bin/python -m uvicorn server:app --host 0.0.0.0 --port 8099
+exec /venv/bin/python -m uvicorn server:app --host 0.0.0.0 --port 8000
 # exec python -m uvicorn server:app --host 0.0.0.0 --port 8099
