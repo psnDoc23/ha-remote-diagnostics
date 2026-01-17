3a5db7fba6dd6f616d03cf1d17ad6139d08ed003
psnDoc23 <dryerson@prophetsports.net>
2026-01-16 17:23:36 -0700

    Update run.sh

--- pha_diagnostics/run.sh
+++ pha_diagnostics/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-echo "PHA Resolve Diagnostic add-on started"
+echo "PHA Guardian Diagnostic add-on started"
 cd /usr/local/bin
 exec /venv/bin/python -m uvicorn server:app --host 0.0.0.0 --port 8099
 # exec python -m uvicorn server:app --host 0.0.0.0 --port 8099
